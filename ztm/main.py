bfb4fc0e9506faaf5ae26b54a009ef3bf7c4d71f
mathheush <mateuszkonopka@icloud.com>
2023-03-16 12:19:55 +0100

    Python Basics [2]

--- ztm/main.py
+++ ztm/main.py
@@ -1,21 +1,106 @@
+# print("Mateusz Konopka")
+# name = input("What is your name?")
+# print("Hi " + name)
+#
+#  Fundamental Data Types
+# int
+# float
+# complex
+# -operators:
+# +
+# -
+# *
+# **
+# /
+# //
+# %
+#
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+#
+#  Classes -> custom types
+#
+#  Specialized Data Types
+#
+# None
+#
+# Math Functions
+# print(round(3.1))
+# print(round(3.5))
+# print(abs(-3))
+
+#  Operator Precedence
+# print(2 + 5 * 3)
+# ()
+# **
+# * /
+# + -
+
+# print(bin(78))
+# print(int("0b101", 2))
+
+# user_iq = 190
+# user_age = user_iq / 4
+# print(user_iq)
+# print(user_age)
+
+#  Constants
+# PI = 3.14
+# a,b,c = 1,2,3
+# print(a,c,b)
+# a += 3
+# print(a)
+
+#  KEYWORDS
+# and	A logical operator
+# as	To create an alias
+# assert	For debugging
+# break	To break out of a loop
+# class	To define a class
+# continue	To continue to the next iteration of a loop
+# def	To define a function
+# del	To delete an object
+# elif	Used in conditional statements, same as else if
+# else	Used in conditional statements
+# except	Used with exceptions, what to do when an exception occurs
+# False	Boolean value, result of comparison operations
+# finally	Used with exceptions, a block of code that will be executed no matter if there is an exception or not
+# for	To create a for loop
+# from	To import specific parts of a module
+# global	To declare a global variable
+# if	To make a conditional statement
+# import	To import a module
+# in	To check if a value is present in a list, tuple, etc.
+# is	To test if two variables are equal
+# lambda	To create an anonymous function
+# None	Represents a null value
+# nonlocal	To declare a non-local variable
+# not	A logical operator
+# or	A logical operator
+# pass	A null statement, a statement that will do nothing
+# raise	To raise an exception
+# return	To exit a function and return a value
+# True	Boolean value, result of comparison operations
+# try	To make a try...except statement
+# while	To create a while loop
+# with	Used to simplify exception handling
+# yield	To end a function, returns a generator
+
+username = "Mateusz"
+password = "Konopka"
+very_long_string = '''
+   /\ 
+  /  \ 
+ /    \ 
+/______\ 
 '''
-#print("Mateusz Konopka")
-#name = input("What is your name?")
-#print("Hi " + name)
+print(very_long_string)
 
-#Fundamental Data Types
-int
-float
-bool
-str
-list
-tuple
-set
-dict
+full_name = username + " " + password
+print(full_name)
 
-#Classes -> custom types
-
-#Specialized Data Types
-
-None
-'''
+print(type(int(str(100))))
