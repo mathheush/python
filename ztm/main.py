e620681dcfc688fa7f8e83bb79721c1cf7475c0c
mathheush <mateuszkonopka@icloud.com>
2023-03-31 23:51:20 +0200

    Python Basics [8]

--- ztm/main.py
+++ ztm/main.py
@@ -303,13 +303,46 @@
 # }
 # print(user.items())
 # print(user[(1,2,3)])
+#
+# my_tuple = (1,2,3,4,5,5,5,5)
+# new_tuple = my_tuple[0:3]
+# print(new_tuple)
+#
+# a,b,c, *other = (1,2,3,4,5)
+# print(other)
+# print(my_tuple.count(5))
+# print(len(my_tuple))
+# print(my_tuple.index(2))
 
-my_tuple = (1,2,3,4,5,5,5,5)
-new_tuple = my_tuple[0:3]
-print(new_tuple)
+# Set
 
-a,b,c, *other = (1,2,3,4,5)
-print(other)
-print(my_tuple.count(5))
-print(len(my_tuple))
-print(my_tuple.index(2))
+# my_set = {1,2,3,4,5,5}
+# my_set.add(100)
+# my_set.add(2)
+# print(my_set)
+# print(1 in my_set)
+# print(len(my_set))
+# new_set = my_set.copy()
+# print(new_set)
+#
+# my_list = [1,2,3,4,5,5]
+# set(my_list)
+# list(my_list)
+# print(my_list)
+
+# my_set = {1,2,3,4,5}
+# your_set = {4,5,6,7,8,9,10}
+#
+# print(my_set.difference(your_set))
+# # my_set.discard(5)
+# # print(my_set)
+# # my_set.difference_update(your_set)
+# # print(my_set)
+# print(my_set.intersection(your_set))
+# print(my_set.isdisjoint(your_set))
+# print(my_set.union(your_set))
+
+# my_set = {4,5}
+# your_set = {4,5,6,7,8,9,10}
+# print(my_set.issubset(your_set))
+# print(your_set.issuperset(my_set))
